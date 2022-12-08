1781a189c9c926cf918ecc9787d23a7244209964
Rekhtina Lyudmila <Rehtina520@mail.ru>
2022-12-08 22:09:25 +0700

    Задача 2

---
+++ Program_01/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+int numberA = 5;
+int numberB = 7;
+
+if(numberA > numberB)
+{
+    Console.WriteLine(numberA);
+}
+else
+{
+    Console.WriteLine(numberB);
+}
